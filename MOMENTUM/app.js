279998be7ef5c17d1a3ca3cdf4d7d2d4819719bd
jiyoonpark <jiyoonp1992@gmail.com>
2023-04-06 17:47:41 +0900

    0406

---
+++ MOMENTUM/app.js
@@ -0,0 +1,13 @@
+//alert("hi");
+console.log(54545454);
+console.log("54545454");
+console.log(5 + 2);
+console.log(5 * 2);
+console.log(5 / 2);
+
+const a = 5;
+const b = 2;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
