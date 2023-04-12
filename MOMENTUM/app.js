53d07cb483a8a951eb34e45666f4a39bf4389a4c
jiyoonpark <jiyoonp1992@gmail.com>
2023-04-12 16:58:40 +0900

    0412

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -7,7 +7,15 @@ console.log(5 / 2);
 
 const a = 5;
 const b = 2;
+let myName = "jiyoon";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
+console.log("Hello " + myName);
+
+myName = jiyoonPark;
+
+console.log("your new name is " + myName);
+
+const amIFat = true;
