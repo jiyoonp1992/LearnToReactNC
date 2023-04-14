360dc8660ab5a68d0b64f701772e154f4b7d3b79
jiyoonpark <jiyoonp1992@gmail.com>
2023-04-14 17:48:32 +0900

    0414

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -19,3 +19,26 @@ myName = jiyoonPark;
 console.log("your new name is " + myName);
 
 const amIFat = true;
+let something;
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri","sat"];
+
+
+//Get Item from Array
+console.log(daysOfWeek);
+console.log(daysOfWeek[5]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+
+const playerName = "nico";
+const playerPoints = 121212;
+const playerHandsome = false;
+const playerFat = "little bit"; 
+
+//const player = ["nico", 1212, false, "little bit"];
+
+//object
+
+const player
