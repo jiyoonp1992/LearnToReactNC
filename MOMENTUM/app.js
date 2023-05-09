65348200cbfe6ea0eb8a406a54f660bd30025cf6
jiyoonpark <jiyoonp1992@gmail.com>
2023-05-09 17:56:43 +0900

    0509

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -11,16 +11,36 @@ const h1 = document.querySelector("div.hello:first-child h1");
 // }
 
 
+// function handleTitleClick(){
+//     const currentColor = h1.style.color;
+//     let newColor;
+//     if(currentColor === "blue"){
+//         newColor = "tomaot";
+//     } else {
+//         newColor = "blue";
+//     }
+//     console.log("title was clicked!");
+//     h1.style.color = newColor;
+// }
+
+// h1.addEventListener("click", handleTitleClick);
+
 function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-    if(currentColor === "blue"){
-        newColor = "tomaot";
-    } else {
-        newColor = "blue";
-    }
-    console.log("title was clicked!");
-    h1.style.color = newColor;
+    //h1.className = "active";
+    const activeClass = "active";
+    // if(h1.className === activeClass){
+    //     h1.className = "";
+    // } else {
+    //     h1.className = activeClass;
+    // }
+
+    // if(h1.classList.contains(activeClass)){
+    //     h1.classList.remove(activeClass);
+    // } else {
+    //     h1.classList.add(activeClass);
+    // }
+
+    h1.classList.toggle(activeClass);
 }
 
-title05.addEventListener("click", handleTitleClick);
+h1.addEventListener("click", handleTitleClick);
