a3de5f65629b4812d9e3ba703607028b6f87f29f
jiyoonpark <jiyoonp1992@gmail.com>
2023-05-12 17:08:09 +0900

    0512

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,4 +1,5 @@
 const loginForm = document.getElementById("login-form");
+const loginForm01 = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
@@ -11,12 +12,31 @@ function onLoginBtnClick(){
     console.log(loginInput.value);
     console.log("hello", loginInput.value);
     const username = loginInput.value;
-    if(username == ""){
-        alert("Please write your name");
-    } else if(username.length > 15){
-        alert("Your name is too long.")
-    }
+    console.log(username);
+    // if(username == ""){
+    //     alert("Please write your name");
+    // } else if(username.length > 15){
+    //     alert("Your name is too long.")
+    // }
     //or form에서 수정
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
+
+
+function onLoginSubmot(event){
+    const username01 = loginInput.value;
+    console.log(username01);
+    console.log(event);
+}
+loginForm.addEventListener("submit", onLoginSubmit);
+
+const link = document.querySelector("a");
+
+function handleLinkClick(event){
+    console.log(event);
+    alert("clicked!");
+    event.preeventDefault();
+}
+
+link.addEventListener("click", handleLinkClick);
