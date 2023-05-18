3822a3ef3673d6982a7060c137f0d9ee5a438152
jiyoonpark <jiyoonp1992@gmail.com>
2023-05-18 16:13:52 +0900

    0518

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -9,12 +9,13 @@ const loginInput01 = document.querySelector("#login-form input");
 const loginButton02 = document.querySelector("#login-form button");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginBtnClick(){
     console.dir(loginInput);
     console.log(loginInput.value);
     console.log("hello", loginInput.value);
-    const username = loginInput.value;
+    const username01 = loginInput.value;
     console.log(username);
     // if(username == ""){
     //     alert("Please write your name");
@@ -30,14 +31,35 @@ function onLoginBtnClick(){
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm01.classList.add(HIDDEN_CLASSNAME);
-    const username01 = loginInput.value;
-    greeting.innerText = "Hello " + username01;
-    greeting.innerText = `Hello ${username01}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    localStorage.getItem("username", username01);
+    const username = loginInput.value;
+    // greeting.innerText = "Hello " + username01;
+    // greeting.innerText = `Hello ${username01}`;
+    // greeting.classList.remove(HIDDEN_CLASSNAME);
+    //localStorage.getItem("username", username01);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+
+//const savedUsername = localStorage.getItem("username");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greeting
+    // greeting.innerText = `Hello ${savedUsername}`;
+    // greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(savedUsername);
+}
 
 // const link = document.querySelector("a");
 
