c845e2f2c9f0b02b819e3b6630006262eba74586
jiyoonpark <jiyoonp1992@gmail.com>
2023-05-31 17:40:16 +0900

    0531

--- MOMENTUM/js/clock.js
+++ MOMENTUM/js/clock.js
@@ -6,7 +6,11 @@ function sayHello(){
 
 function getClock(){
     const date = new Date();
-    clock.innerText = `${date.getHours}:${date.getMinutes}:${date.getSeconds}`;
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    //clock.innerText = `${date.getHours}:${date.getMinutes}:${date.getSeconds}`;
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 };
 
 setInterval(sayHello, 5000);
