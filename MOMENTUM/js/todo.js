05cf041f019186380e9d15082c40565f28c17dd7
jiyoonpark <jiyoonp1992@gmail.com>
2023-06-08 17:37:15 +0900

    0608

--- MOMENTUM/js/todo.js
+++ MOMENTUM/js/todo.js
@@ -1,9 +1,42 @@
 const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+const toDos = [];
+
+function saveToDos(){
+    //localStorage.setItem("todos", toDos);
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function deleteTodo(event){
+    console.log(event);
+    console.log(event.target);
+    console.dir(event.target.parentElement.innerText);
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+
+}
 
 function handleToDoSubmit(event){
     event.preventDefault();
-
+    console.log(toDoInput.value);
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    toDos.push(newTodo);
+    paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
